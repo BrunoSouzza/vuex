bff8ad228f6538033b0861df6d10e556ea8badb0
BrunoSouzza <souza_0010@hotmail.com>
2021-09-12 12:45:24 -0300

    created project

---
+++ src/components/Loja.vue
@@ -0,0 +1,69 @@
+<template>
+    <Painel titulo="Loja Virtual" verde>
+        <div class="loja">
+            <span>Adicionar</span>
+            <input type="number" v-model.number="quantidade">
+            <span>itens de <strong>R$</strong></span>
+            <input type="number" v-model.number="preco">
+            <button @click="adicionar">Agora!</button>
+        </div>
+    </Painel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sequencia: 1,
+    };
+  },
+  computed: {
+    quantidade: {
+      get() {
+        return this.$store.state.quantidade;
+      },
+      set(value) {
+        this.$store.commit('setQuantidade', value);
+      },
+    },
+    preco: {
+      get() {
+        return this.$store.state.preco;
+      },
+      set(value) {
+        this.$store.commit('setPreco', value);
+      },
+    },
+  },
+  methods: {
+    adicionar() {
+      const produto = {
+        id: this.sequencia,
+        nome: `Produto ${this.sequencia}`,
+        quantidade: this.quantidade,
+        preco: this.preco,
+      };
+      this.sequencia++;
+      // eslint-disable-next-line
+      console.log(produto);
+      this.$store.dispatch('adicionarProduto', produto);
+    },
+  },
+};
+</script>
+
+<style>
+.loja {
+  display: flex;
+  justify-content: center;
+}
+
+.loja > * {
+  margin: 0px 10px;
+}
+
+input {
+  font-size: 2rem;
+  width: 90px;
+}
+</style>
---
+++ src/components/Resumo.vue
@@ -0,0 +1,41 @@
+<template>
+    <Painel titulo="Resumo" roxo>
+        <div class="resumo">
+            <span>Total: <strong>{{ total | dinheiro }}</strong></span>
+            <hr>
+            <button>Finalizar!</button>
+        </div>
+    </Painel>
+</template>
+
+<script>
+//import { mapGetters } from 'vuex';
+export default {
+  //   computed: {
+  //     ...mapGetters({
+  //       total: 'valorTotal',
+  //     }),
+  //   },
+
+  //Leitura dos GETTERS usando MAPGETTERS
+  //computed: mapGetters(['valorTotal']),
+
+  // Leitura dos GETTERS diretamente - Mais comum
+  computed: {
+    total() {
+      return this.$store.getters.valorTotal;
+    },
+  },
+};
+</script>
+
+<style>
+table {
+  width: 100%;
+}
+
+td {
+  border-top: 1px solid #eee;
+  width: 33%;
+}
+</style>
