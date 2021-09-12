d73c68e006c8d70b822f2b968a588235dc1e67cb
BrunoSouzza <souza_0010@hotmail.com>
2021-09-12 16:24:39 -0300

    separating store into files.

--- src/store/store.js
+++ src/store/store.js
@@ -1,37 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import carrinho from './modules/carrinho';
+import parametros from './modules/parametros';
+
+import * as getters from './getters';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    produtos: [],
-    quantidade: 2,
-    preco: 19.99,
+    nome: 'Bruno',
+    sobrenome: ' Souza',
   },
-  getters: {
-    valorTotal(state) {
-      return state.produtos
-        .map((p) => p.quantidade * p.preco)
-        .reduce((total, atual) => total + atual, 0);
-    },
-  },
-  mutations: {
-    adicionarProduto(state, payload) {
-      state.produtos.push(payload);
-    },
-    setQuantidade(state, payload) {
-      state.quantidade = payload;
-    },
-    setPreco(state, payload) {
-      state.preco = payload;
-    },
-  },
-  actions: {
-    adicionarProduto({ commit }, payload) {
-      setTimeout(() => {
-        commit('adicionarProduto', payload);
-      }, 5000);
-    },
-  },
+  getters,
+  modules: { carrinho, parametros },
 });
